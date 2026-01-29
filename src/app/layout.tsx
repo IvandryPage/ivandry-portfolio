9128b58a6a52bd147e1274dcaec1397ceecfd09a
IvandryPage <ivandry.89@gmail.com>
2026-01-29 08:58:04 +0700

    chore: initialize portfolio setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import {   JetBrains_Mono, Space_Grotesk, Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+
+const jetBrainsMono = JetBrains_Mono({
+  variable: '--font-jetbrains',
+  subsets: ["latin"],
+  display: 'swap'
+})
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  variable: '--font-jakarta',
+  subsets: ["latin"],
+  display: 'swap'
+})
+
+const spaceGrostek = Space_Grotesk({
+  variable: "--font-space-grostek",
+  subsets: ["latin"],
+  display: 'swap'
+});
+
+export const metadata: Metadata = {
+  title: "Ivandry Portfolio",
+  description: "Portfolio Website for Showcasing the Ivandry's Works",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${plusJakartaSans.variable} ${jetBrainsMono.variable} ${spaceGrostek.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function LandingPage() {
+  return (
+    <h1 className="w-full flex items-center justify-center h-screen">Ivandry Portfolio</h1>
+  );
+}
